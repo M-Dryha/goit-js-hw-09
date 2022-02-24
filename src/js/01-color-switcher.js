e70de10aefde04c421b7ea6223d124f0d07d6e8d
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-24 20:11:50 +0300

    add task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyElem = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+bodyElem.style.backgroundColor = getRandomHexColor();
+ 
+let disabledBtn = false;
+let timerId = null;
+
+function onStartBtn() {
+
+    if (disabledBtn) {
+        return;
+}
+  timerId = setInterval(() => {
+        console.log('Маша - молодец!');
+        let newColor = getRandomHexColor();
+        bodyElem.style.backgroundColor = newColor;
+    }, 1000)
+   disabledBtn = true;
+}
+
+function onStopBtn() {
+
+    clearInterval(timerId);
+    disabledBtn = false;
+}
 
